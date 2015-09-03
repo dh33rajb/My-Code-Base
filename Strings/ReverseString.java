0d4cd399be1ff2e8eee1da574a1453ea88ca0daf
Dheeraj Balakavi <satyacha@buffalo.edu>
2015-09-03 11:47:48 -0400

    Create ReverseString.java

---
+++ Strings/ReverseString.java
@@ -0,0 +1,28 @@
+// Reverse a string using java
+// Reverse a string using java
+
+import java.util.*;
+import java.lang.*;
+
+public class ReverseString {
+    public static void main (String args[]) {
+        // Step-1
+        Scanner scanner = new Scanner (System.in);
+        String input = scanner.nextLine();
+        
+        // Step-2: Core logic
+        String output = reverseString (input, 0);
+        System.out.println ("Input String: " + input + " and Output String: " + output);
+    }
+    
+    public static String reverseString (String input, int count) {
+        if (input == null || input.length() < 2)
+            return input;
+        String output = "";
+        if (count == (input.length()-1))
+            return String.valueOf(input.charAt(count));
+        output = reverseString (input, count + 1) + input.charAt(count);
+        return output;
+    }
+    
+}
