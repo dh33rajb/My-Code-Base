86940d15af6a24b05ec9d5475ca88d605a2255ce
Dheeraj Balakavi <satyacha@buffalo.edu>
2015-09-05 21:24:56 -0400

    Update AllStringPermutationsDP.java

--- AllStringPermutationsDP.java
+++ AllStringPermutationsDP.java
@@ -1,1 +1,41 @@
 // Find all permutations of a string using DYNAMIC PROGRAMMING
+/*
+Input (Hardcoded):
+abc
+
+Output:
+abc
+acb
+bac
+bca
+cba
+cab
+*/
+public class AllStringPermutationsDP {
+	public static void main(String args[]) {
+		String input = "abc";
+		char[] inputArr = input.toCharArray();
+		printAllPermutations(inputArr, 0, inputArr.length);
+	}
+
+	public static void printAllPermutations(char[] inputArr, int startIndex,
+			int endIndex) {
+		if (startIndex == endIndex) {
+			String output = "";
+			for (int i = 0; i < endIndex; i++)
+				output = output + inputArr[i];
+			System.out.println(output);
+		}
+		for (int i = startIndex; i < endIndex; i++) {
+			char temp = inputArr[startIndex];
+			inputArr[startIndex] = inputArr[i];
+			inputArr[i] = temp;
+			printAllPermutations(inputArr, startIndex + 1, endIndex);
+			temp = inputArr[startIndex];
+			inputArr[startIndex] = inputArr[i];
+			inputArr[i] = temp;
+		}
+
+	}
+}
+
