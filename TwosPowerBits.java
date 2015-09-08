e4dd6418f7da7e9c816eb68683bf1ddf69e3dd58
Dheeraj Balakavi <satyacha@buffalo.edu>
2015-09-08 00:54:10 -0400

    Update TwosPowerBits.java

--- TwosPowerBits.java
+++ TwosPowerBits.java
@@ -1,3 +1,29 @@
-// find if a number is a power of 2..
-ex: 2,4,8,16,32.. are powers of two ==> return true
+/* FIND IF A NUMBER IS A POWER OF 2
+Ex: 
+---
+2,4,8,16,32.. are powers of two ==> return true
 1,6,12.. are not powers of two ==> return false
+*/
+import java.util.*;
+import java.lang.*;
+
+public class TwosPowerBits {
+  public static void main (String args[]) {
+    int yesInput = 32;
+    int noInput = 12;
+    boolean yesResult = isPowerOfTwo (yesInput);
+    boolean noResult = isPowerOfTwo (noInput);
+    System.out.println (yesResult + "-" + noResult);
+  }
+  public static boolean isPowerOfTwo (int number) {
+    int count = 0;
+    while (number > 0) {
+      number = number & (number -1);
+      count++;
+    }
+    if (count == 1)
+      return true;
+    else
+      return false;
+  }
+}
