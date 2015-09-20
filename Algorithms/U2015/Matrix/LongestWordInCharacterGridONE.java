16f384acc7deb90dbc27981d52b24ac97924714a
Dheeraj Balakavi <satyacha@buffalo.edu>
2015-09-20 19:29:21 -0400

    Update LongestWordInCharacterGridONE.java

--- Algorithms/U2015/Matrix/LongestWordInCharacterGridONE.java
+++ Algorithms/U2015/Matrix/LongestWordInCharacterGridONE.java
@@ -1,21 +1,30 @@
 // Find the longest word in a grid of random letter, each consecutive letters has to be next to the previous one.
-// IF THE CONSECUTIVE STRINGS "CANNOT" SPAN OVER NEXT ROWS
+// IF THE CONSECUTIVE STRINGS "CANNOT" and "CAN" SPAN OVER NEXT ROWS.. BOTH CASES HANDLED HERE
 /*
+IF CONSECUTIVE STRINGS "CANNOT" SPAN ACROSS ROWS
 Input:
 ------
 char grid[][]= {{'q', 'w', 'e', 'r', 't', 'y'},
                 {'a', 's', 'd', 'f', 'g', 'h'},
                 {'z', 'x', 'c', 'v', 'b', 'n'},
-                {'a', 'b', 'c', 'd', 'e', 'n'},
+                {'a', 'b', 'c', 'd', 'e', 'f'},
                 {'l', 'm', 'n', 'o', 'p', 'q'}};
                 
 Output:
 -------
-Longest consecutive letter string: lmnopq
+Longest consecutive letter string: abcdef
+
+IF CONSECUTIVE STRINGS "CAN" SPAN ACROSS ROWS
+char grid[][]= {{'q', 'w', 'e', 'r', 't', 'y'},
+                {'a', 's', 'd', 'f', 'g', 'h'},
+                {'z', 'x', 'c', 'v', 'b', 'n'},
+                {'a', 'b', 'c', 'd', 'e', 'f'},
+                {'l', 'm', 'n', 'o', 'p', 'q'},
+                {'r', 'm', 'n', 'o', 'p', 'q'}};
 
 output:
 -------
-a  b  c  d  e
+Longest consecutive letter string: lmnopqr
 */
 import java.lang.*;
 import java.util.*;
@@ -27,10 +36,17 @@ public class LongestWordInCharacterGridONE {
     char grid[][]= {{'q', 'w', 'e', 'r', 't', 'y'},
                     {'a', 's', 'd', 'f', 'g', 'h'},
                     {'z', 'x', 'c', 'v', 'b', 'n'},
-                    {'a', 'b', 'c', 'd', 'e', 'n'},
-                    {'l', 'm', 'n', 'o', 'p', 'q'}};
+                    {'a', 'b', 'c', 'd', 'e', 'f'},
+                    {'l', 'm', 'n', 'o', 'p', 'q'},
+                    {'r', 'f', 'j', 'k', 't', 'y'}};
     
     // Step-2: Core logic
+    /* working code here is for CANNOT span across rows case
+       for CAN span across rows case, pull the below code snippet and put it ouside the outer for loop.. all will work weell then
+       
+      char prevChar = ' ';
+      String localMaxString = "";
+    */
     String maxString = "";
     for (int i=0; i < grid.length; i++) {
       char prevChar = ' ';
