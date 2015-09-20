ca0f6339ed06b67cf30234f9b7b982095ff64a73
Dheeraj Balakavi <satyacha@buffalo.edu>
2015-09-19 22:24:34 -0400

    Update RotateSquareMatrixClockwiseNinety.java

--- Algorithms/U2015/RotateSquareMatrixClockwiseNinety.java
+++ Algorithms/U2015/RotateSquareMatrixClockwiseNinety.java
@@ -1,7 +1,23 @@
+// ROTATE SQUARE MATRIX CLOCKWISE BY NINETY
+/*
+Input:
+------
+0	1	2	3	4	
+5	6	7	8	9	
+10	11	12	13	14	
+15	16	17	18	19	
+20	21	22	23	24	
+
+Output:
+-------
+20 15 10 5 0 
+21 16 11 6 1 
+22 17 12 7 2 
+23 18 13 8 3 
+24 19 14 9 4 
+*/
 import java.util.ArrayList;
 import java.util.List;
-
-// ROTATE SQUARE MATRIX CLOCKWISE BY NINETY
 
 public class RotateSquareMatrixClockwiseNinety {
   public static void main(String args[]) {
