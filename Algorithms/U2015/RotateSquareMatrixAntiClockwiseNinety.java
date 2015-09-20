f6739206b21b6ba5ea4b84d000a47d974d64e989
Dheeraj Balakavi <satyacha@buffalo.edu>
2015-09-19 22:23:11 -0400

    Update and rename RotateMatrixAntiClockwiseNinety.java to RotateSquareMatrixAntiClockwiseNinety.java

--- Algorithms/U2015/RotateSquareMatrixAntiClockwiseNinety.java
+++ Algorithms/U2015/RotateSquareMatrixAntiClockwiseNinety.java
@@ -1,9 +1,27 @@
+// ROTATE SQUARE MATRIX ANTI-CLOCKWISE BY NINETY
+
+/*
+Input:
+------
+0	1	2	3	4	
+5	6	7	8	9	
+10	11	12	13	14	
+15	16	17	18	19	
+20	21	22	23	24	
+
+Output:
+------
+4 9 14 19 24 
+3 8 13 18 23 
+2 7 12 17 22 
+1 6 11 16 21 
+0 5 10 15 20 
+*/
+
 import java.util.ArrayList;
 import java.util.List;
 
-// ROTATE SQUARE MATRIX ANTI-CLOCKWISE BY NINETY
-
-public class RotateMatrixAntiClockwiseNinety {
+public class RotateSquareMatrixAntiClockwiseNinety {
   public static void main(String args[]) {
     // Step-1: Reading inputs
     int rows = 5; int counter=0;
