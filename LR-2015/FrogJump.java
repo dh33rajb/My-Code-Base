68dc68eceecce2f2bda52a2bd4d33d45c5e68ab7
Dheeraj Balakavi <satyacha@buffalo.edu>
2015-10-06 23:14:08 -0400

    Create FrogJump.java

---
+++ LR-2015/FrogJump.java
@@ -0,0 +1,46 @@
+import java.util.Map;
+import java.util.TreeMap;
+
+public class FrogJump {
+	public static void main(String args[]) {
+		System.out.println(frogJump(new int[] {1, 3, 4, 5, 6, 7, 9, 1, 4, 5, 6}, 10, 2));
+	}
+
+	public static int frogJump(int[] arr, int X, int D) {
+		// Step-0: Base condition
+		if (arr == null || arr.length == 0 || X <= D)
+			return 0;
+
+		// Step-1: Create TreeMap ==> key = sorted positions; value = time leaf falls on the position
+		Map<Integer, Integer> map = new TreeMap<Integer, Integer>();
+		map.put(0, 0);
+		map.put(X, 0);
+		for (int i = 0; i < arr.length; i++) {
+			if (map.get(arr[i]) == null)
+				map.put(arr[i], i);
+		}
+		System.out.println(map);
+
+		// Step-2: Update TreeMap ==> key = sorted positions; value = best overall time to reach the position
+		for (Map.Entry<Integer, Integer> e : map.entrySet()) {
+			int pos = e.getKey();
+			int timeLeafFalls = e.getValue();
+			if (pos > D) {
+				int bestTime = Integer.MAX_VALUE;
+				for (int step = 1; step <= D; step++) {
+					if (map.get(pos - step) == null)
+						continue;
+					if (map.get(pos - step) < timeLeafFalls) {
+						bestTime = timeLeafFalls;
+						break;
+					} else
+						bestTime = Math.min(map.get(pos - step), bestTime);
+				}
+				// e.setValue(bestTime);
+				map.put(pos, bestTime);
+			}
+		}
+		System.out.println(map);
+		return map.get(X);
+	}
+}
