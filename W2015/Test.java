d9dfb641b17db6a877e3f829702d1a6170e8517f
Dheeraj Balakavi <satyacha@buffalo.edu>
2015-10-11 00:26:32 -0400

    Create Test.java

---
+++ W2015/Test.java
@@ -0,0 +1,23 @@
+import java.util.Hashtable;
+
+public class Test {
+
+	public static void main(String args[]) {
+		// (int + long) * (double + float)
+		int a = 10;
+		long b = 122222222333L;
+		float c = 123.45f;
+		double d = 123434343434343434.455;
+
+		double result = (a + b) * (c + d);
+		System.out.println(result);
+
+	}
+
+	public static void testComments() {
+		// /* this is a // // come */
+		/* dafdsa *** */
+		/* dafdfa /* dafd */*/
+		/* afadd // adfad */
+	}
+}
