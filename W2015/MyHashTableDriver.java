bacdcc01a8d606ab10b04c1053415de7ca879904
Dheeraj Balakavi <satyacha@buffalo.edu>
2015-10-11 00:25:48 -0400

    Update and rename MyHashTable.java to MyHashTableDriver.java

--- W2015/MyHashTableDriver.java
+++ W2015/MyHashTableDriver.java
@@ -3,7 +3,7 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
-// My equivalent of HashTable class
+// My equivalent of Hashtable class
 class MyHashTable<K, V> {
 
 	private int capacity;
@@ -18,6 +18,8 @@ class MyHashTable<K, V> {
 	// Constructor with user-defined capacity
 	public MyHashTable(int capacity) {
 		this.capacity = capacity;
+		// ArrayList is used to hold the table as capacity can be pre-defined in
+		// ArrayLists
 		this.cells = new ArrayList<List<MyEntry<K, V>>>(this.capacity);
 		int count = -1;
 		while (++count < this.capacity)
@@ -26,12 +28,13 @@ class MyHashTable<K, V> {
 
 	// Put method of MyHashTable
 	public void put(K key, V value) {
-		if (key == null)
-			throw new NullPointerException("Null keys are not allowed");
+		if (key == null || value == null)
+			throw new NullPointerException("Null keys/values are not allowed");
 
 		int cellIndex = this.hash(key);
 		List<MyEntry<K, V>> cell = this.cells.get(cellIndex);
 		if (cell == null)
+			// LinkedList is used for each cell to chain keys during collisions
 			cell = new LinkedList<MyEntry<K, V>>();
 		else {
 			// If key already exists, perform concurrent modification of value
@@ -69,9 +72,10 @@ class MyHashTable<K, V> {
 		return null;
 	}
 
-	// A simple Hash Function for MyHashTable
+	// Hash Function for MyHashTable.
+	// The Hash Function is the same used in Java SDK HashMap class
 	public int hash(K key) {
-		return (key.hashCode() % this.capacity);
+		return ((key.hashCode() & 0x7FFFFFFF) % this.capacity);
 	}
 
 	// My equivalent of Map.Entry class (a nested class)
@@ -97,9 +101,12 @@ public class MyHashTableDriver {
 		myHashTable.put("a", 11);
 		myHashTable.put("b", 12);
 		myHashTable.put("c", 13);
+		// myHashTable.put("d", null);
 		System.out.println(myHashTable.get("a"));
 		System.out.println(myHashTable.get("b"));
 		System.out.println(myHashTable.get("c"));
+		System.out.println(myHashTable.get("d"));
+		System.out.println(myHashTable.get("e"));
 
 		MyHashTable<Integer, Integer> myOtherHashTable = new MyHashTable<Integer, Integer>(
 				1);
@@ -109,13 +116,11 @@ public class MyHashTableDriver {
 		myOtherHashTable.put(1, 111);
 		myOtherHashTable.put(2, 112);
 		myOtherHashTable.put(3, 113);
+		// myOtherHashTable.put(4, null);
 		System.out.println(myOtherHashTable.get(1));
 		System.out.println(myOtherHashTable.get(2));
 		System.out.println(myOtherHashTable.get(3));
-
-		MyHashTable<String, String> myAnotherHashTable = new MyHashTable<String, String>(
-				1);
-		myAnotherHashTable.put(null, "a");
-		System.out.println(myAnotherHashTable.get(null));
+		System.out.println(myOtherHashTable.get(4));
+		System.out.println(myOtherHashTable.get(5));
 	}
 }
