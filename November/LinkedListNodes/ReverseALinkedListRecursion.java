0969c43907bf8b787c1df09d3da9c488e64d2a16
Dheeraj Balakavi <satyacha@buffalo.edu>
2015-11-01 18:11:38 -0500

    Update ReverseALinkedListRecursion.java

--- November/LinkedListNodes/ReverseALinkedListRecursion.java
+++ November/LinkedListNodes/ReverseALinkedListRecursion.java
@@ -9,6 +9,11 @@
 public class Solution {
     private ListNode tail = null;
     public ListNode reverseList(ListNode head) {
+        reverseListRecurse(head);
+        return tail;
+    }
+    
+    public ListNode reverseListRecurse (ListNode head) {
         if (head == null)
           return head;
         ListNode next = reverseList (head.next);
