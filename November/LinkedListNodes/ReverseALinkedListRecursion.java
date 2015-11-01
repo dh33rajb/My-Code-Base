120389d7eacaac60cfcd5d9acb4a3634bc24519c
Dheeraj Balakavi <satyacha@buffalo.edu>
2015-11-01 18:09:28 -0500

    Create ReverseALinkedListRecursion.java

---
+++ November/LinkedListNodes/ReverseALinkedListRecursion.java
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    private ListNode tail = null;
+    public ListNode reverseList(ListNode head) {
+        if (head == null)
+          return head;
+        ListNode next = reverseList (head.next);
+        if (next == null)
+          tail = head;
+        else
+          next.next = head;
+        head.next = null;
+        return head;
+    }
+}
